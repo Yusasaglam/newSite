fa27e75ef00e21278b6c03e3da3875a9172b6874
mevlutsaglam68@gmail.com <mevlutsaglam68@gmail.com>
2025-11-27 21:39:09 +0300

    responsiveTam

---
+++ js/include.js
@@ -0,0 +1,14 @@
+AOS.init();
+
+// Scroll efekti
+const navbar = document.getElementById('mainNavbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+
